c8892de394a0d109a158061216417ef9da4e60cf
Luther <luther1096@gmail.com>
2025-02-16 02:00:41 +0530

    json server setup + fetching questions data on app startup + used reducer to store state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        dispatch({ type: "dataReceived", payload: data });
+      })
+      .catch((err) => {
+        console.error(err);
+        dispatch({ type: "dataFailed" });
+      });
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>5/10</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
